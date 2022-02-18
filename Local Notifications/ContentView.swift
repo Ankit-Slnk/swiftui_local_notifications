3697c0113149f468e816d3e3792f96517d725f40
Ankit Solanki <ankit_ms95@yahoo.com>
2022-02-19 00:24:36 +0530

    first commit

--- Local Notifications/ContentView.swift
+++ Local Notifications/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Send Notification") {
+            sendNotification()
+        }
+    }
+    
+    public func sendNotification() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                print("All set!")
+                
+                let content = UNMutableNotificationContent()
+                content.title = "Notification Titla"
+                content.subtitle = "subtitle"
+                content.body = " Notification triggered"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    if error != nil {
+                        print(error)
+                        // Handle any errors.
+                    }
+                }
+                
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
--- Local Notifications/Local_NotificationsApp.swift
+++ Local Notifications/Local_NotificationsApp.swift
@@ -6,12 +6,69 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct Local_NotificationsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in }
+            )
+        } else {
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // if app open and notfication receives
+        print("userNotificationCenter 1")
+        print(notification)
+        print(notification.request.content.title)
+        print(notification.request.content.subtitle)
+        completionHandler([[.banner, .sound]]) // comment to not show notification
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // if app in background and tap on notification
+        print("userNotificationCenter 2")
+        print(response)
+        print(response.targetScene!.title!)
+        print(response.targetScene!.subtitle)
+        completionHandler()
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("\(#function)")
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification notification: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("\(#function)")
+    }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        print("\(#function)")
+        return false
     }
 }

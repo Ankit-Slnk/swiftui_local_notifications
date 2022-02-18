62ce495f5175de8c8a2a18ddd54be8d51bb4b5d8
Ankit Solanki <ankit_ms95@yahoo.com>
2022-02-18 23:52:12 +0530

    Initial Commit

---
+++ Local Notifications/Local_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_NotificationsApp.swift
+//  Local Notifications
+//
+//  Created by Ankit Solanki on 18/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Local_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
